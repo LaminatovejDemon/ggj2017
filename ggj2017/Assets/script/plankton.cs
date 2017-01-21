8a3247b2edebe0aaf3d3b8585c1cb8a15c979723
Plysovej Kaktus <filip.pokorny@solfa.uk>
2017-01-21 14:32:46 +0000

    FP: plankton, water gets darker in deep

---
+++ ggj2017/Assets/script/plankton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plankton : MonoBehaviour {
+
+	planktonManager _manager;
+	float _initTimestamp;
+	float _lifeSpan;
+	Vector3 _velocity;
+	float _maxAlpha;
+
+	public void Initialise(planktonManager manager){
+		_manager = manager;
+
+		Vector3 randomViewport_ = new Vector3 ((Random.value) * 1.0f, (Random.value) * 1.0f, Random.Range(3.0f, 20.0f));
+		transform.position = Camera.main.ViewportToWorldPoint( randomViewport_);
+//		Debug.Log ("position is " + transform.position + " ( " + randomViewport_ + ")");
+//		transform.position += Vector3.forward * Random.Range(10.0f, 20.0f);
+		float _scale_ = Random.Range (0.02f, 0.8f);
+		_initTimestamp = Time.time;
+		_maxAlpha = Random.Range (0.001f, 0.04f) / _scale_;
+		_velocity = new Vector3 (Random.Range (-1.0f, 1.0f), Random.Range (-1.0f, 1.0f), 0) * 0.05f / _scale_;
+		_lifeSpan = Random.Range (2.0f, 5.0f);
+		while (transform.position.y > -10.0f) {
+			Vector3 bak_ = transform.position;
+			bak_.y -= 5.0f;
+			transform.position = bak_;
+		}
+		transform.localScale = Vector3.one * _scale_;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - _initTimestamp > _lifeSpan) {
+			Initialise (_manager);
+		}
+		transform.position += _velocity * Time.deltaTime;
+		Color bak_ = GetComponent<Renderer> ().material.GetColor ("_TintColor");
+		bak_.a = (0.5f - Mathf.Abs (0.5f - ((Time.time - _initTimestamp) / _lifeSpan))) * _maxAlpha * 2.0f;
+		GetComponent<Renderer> ().material.SetColor("_TintColor", bak_);
+
+	/*	Vector3 viewport_ = Camera.main.WorldToViewportPoint (transform.position);
+		Debug.Log (viewport_);
+		if (viewport_.x > 1.5f || viewport_.x < -1.5f || viewport_.y > 1.5f || viewport_.y < -1.5f) {
+			Initialise (_manager);
+		}*/
+	}
+}
---
+++ ggj2017/Assets/script/planktonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planktonManager : MonoBehaviour {
+
+	public plankton [] planktonTemplates;
+	plankton[] _planktonInstances;
+	public int _planktonCount = 1;
+
+	void InitialisePlankton(){
+		if (_planktonInstances != null ) {
+			return;
+		}
+		_planktonInstances = new plankton[_planktonCount];
+
+		for (int i = 0; i < _planktonCount; ++i) {
+			_planktonInstances [i] = GameObject.Instantiate (planktonTemplates [Random.Range (0, planktonTemplates.Length)]);
+			_planktonInstances [i].Initialise (this);
+		}
+	}		
+
+	void Update(){
+		InitialisePlankton ();
+	}
+}
