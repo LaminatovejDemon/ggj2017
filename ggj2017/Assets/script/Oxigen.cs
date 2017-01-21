1bed2c55e7fd4653370fd119c4ff78d80de7b5a9
Juan Jose Rebollo <jrebollo@jaguarlandrover.com>
2017-01-21 16:56:59 +0000

    Oxigen label introduced.

---
+++ ggj2017/Assets/script/Oxigen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oxigen : MonoBehaviour {
+
+	public TextMesh _oxigenTemplate;
+	TextMesh _oxigen;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InitialiseOxigen ();
+		UpdateOxigen ();
+	}
+
+	void InitialiseOxigen () {
+		if (_oxigen != null) {
+			return;
+		}
+
+		_oxigen = new TextMesh ();
+		_oxigen= GameObject.Instantiate (_oxigenTemplate);
+		Vector3 v3Pos = new Vector3(1.2f, 1.1f, 10.0f);
+		_oxigen.transform.parent = Camera.main.transform;
+		_oxigen.transform.position = Camera.main.ViewportToWorldPoint(v3Pos);
+		_oxigen.transform.position += Vector3.forward * 5.0f;
+	}
+
+	void UpdateOxigen () {
+
+	}
+}
